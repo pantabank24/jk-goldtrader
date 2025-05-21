5ec4b070e747e44603b290ddd559652b6f7779da
pantabank24 <150137441+pantabank24@users.noreply.github.com>
2025-05-22 02:57:19 +0700

    updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -63,7 +63,7 @@ export const Navbar = () => {
   );
 
   return (
-    <HeroUINavbar className=" bg-[#710711]" maxWidth="xl" position="sticky" >
+    <HeroUINavbar className=" bg-[#710711]" maxWidth="xl" position="sticky"  >
       <NavbarContent className="basis-1/5 sm:basis-full " justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
         <NextLink className="flex justify-start items-center gap-1" href="/">
