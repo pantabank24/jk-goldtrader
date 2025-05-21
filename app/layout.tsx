062da67a5ddf5d06bf9d5a74c21af3f5ae8aa5b2
pantabank24 <150137441+pantabank24@users.noreply.github.com>
2025-05-22 02:59:36 +0700

    ...

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    images: {
+      url: "https://jk-goldtrader-rhfc.vercel.app/images/jk-banner.jpg",
+      width: 1200,
+      height: 630,
+    }
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      {children}
+    </html>
+  );
+}
