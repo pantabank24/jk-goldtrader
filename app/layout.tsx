4470556facfe65bbefcae8a7c966508a7231c729
pantabank24 <150137441+pantabank24@users.noreply.github.com>
2025-07-23 22:06:58 +0700

    remove ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <AdPopup/>
+            {/* <AdPopup/> */}
             <AddToHomeScreenPrompt/>
 
             <main className="">
